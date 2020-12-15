84599e4c3e8dcd51d15eb5a4bd8141c2d17479b0
Lapasrada-p <76034405+Lapasrada-p@users.noreply.github.com>
2020-12-15 15:24:18 +0700

    lab5_1.cpp completed

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -2,8 +2,21 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int x;
+    int odd =0;
+    int even =0;
+    while(x!=0){
+        cout << "Enter an integer: ";
+        cin >> x;
+        if (x%2==0){
+            if (x!=0){
+            even+=1;
+            }
+        }else{
+            odd+=1;
+        }
+    }
+    cout << "#Even numbers = " <<even <<"\n";
+    cout << "#Odd numbers = " <<odd;
     return 0;
 }
