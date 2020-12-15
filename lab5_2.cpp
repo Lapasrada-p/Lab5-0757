f8f1ddfe4320b29bc377f9cb873b439334d55828
Lapasrada-p <76034405+Lapasrada-p@users.noreply.github.com>
2020-12-15 16:16:39 +0700

    lab5_2.cpp completed

--- lab5_2.cpp
+++ lab5_2.cpp
@@ -1,6 +1,42 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
+
+double deg2rad(double angleDeg){
+    double angleRad1;
+    angleRad1 = angleDeg*M_PI/180.0 ;
+    return angleRad1;
+}
+double rad2deg(double angleRad){
+    double angleDeg1;
+    angleDeg1 = angleRad*180.0/M_PI ;
+    return angleDeg1;
+}
+double findXComponent(double l1,double l2,double rad1,double rad2){
+    double x1;
+    double x2;
+    x1 =l1*cos(rad1);
+    x2 =l2*cos(rad2);
+    return x1+x2;
+}
+double findYComponent(double l1,double l2,double rad1,double rad2){
+    double y1;
+    double y2;
+    y1 =l1*sin(rad1);
+    y2 =l2*sin(rad2);
+    return y1+y2;
+}
+double pythagoras(double X, double Y){
+    double XY;
+    XY = sqrt(pow(X,2) + pow(Y,2));
+    return XY;
+}
+void showResult(double ANSside,double ANSangle){
+    cout<<"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"<<"\n";
+    cout<<"Length of the resultant vector = "<< ANSside <<"\n";
+    cout<<"Direction of the resultant vector (deg) = "<< ANSangle<<"\n";
+    cout<<"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%";
+}
 
 int main(){
     double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
